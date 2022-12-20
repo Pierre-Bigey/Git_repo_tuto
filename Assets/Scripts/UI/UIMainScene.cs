ce25b07f8d0febe048c0a8b3afeea1913e35cd34
Pierre Bigey <pierre.bigey1@gmail.com>
2022-12-20 22:11:25 +0100

    created a scene flow

--- Assets/Scripts/UI/UIMainScene.cs
+++ Assets/Scripts/UI/UIMainScene.cs
@@ -29,6 +29,11 @@ public class UIMainScene : MonoBehaviour
         ResourceDB.Init();
     }
 
+    public void Menu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
     private void OnDestroy()
     {
         Instance = null;
